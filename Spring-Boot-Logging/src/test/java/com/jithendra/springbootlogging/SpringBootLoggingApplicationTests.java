fc9cbdde6f098b5a42d9ce153f0efa3f3127b93e
Jithendra <jithendrads@gmail.com>
2021-01-01 11:53:35 +0530

    Spring boot logging example

---
+++ Spring-Boot-Logging/src/test/java/com/jithendra/springbootlogging/SpringBootLoggingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jithendra.springbootlogging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLoggingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
